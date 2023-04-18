2e931c8153d33489060ce0650205d4151215b82f
mehdiinsy <126080136+mehdiinsy@users.noreply.github.com>
2023-04-18 11:47:53 +0200

    connection

--- boo.js
+++ boo.js
@@ -1,6 +1,31 @@
+// ===========================================================================Method post boo
 function stocker(){
-    localStorage.setItem("prenom",document.getElementById("prenom").value);
+    localStorage.setItem("mdp",document.getElementById("mdp").value);
     localStorage.setItem("nom",document.getElementById("nom").value);
-    window.location= "deux.html";
+    window.location= "login.html";
 }
 
+// function charger(){
+//     document.getElementById("essai").innerHTML = localStorage.getItem("prenom") + " " + localStorage.getItem("nom");
+// }
+
+// ==============================================================================page de co 
+function connection(){
+
+
+    let p1= localStorage.getItem("nom")
+    let m1= localStorage.getItem("mdp")
+    let pseudo= document.getElementById("Identifiant").value
+    let mdp= document.getElementById("mdp_co").value
+
+    if(p1 === pseudo && m1===mdp){
+        window.location = "yes.html"}
+
+    else{
+        alert("Mot de passe ou identifiant faux")
+    }
+}
+
+function annuler(){
+    window.location= "boo.html"
+}
